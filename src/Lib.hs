34cd140018cb987b4af05cb5c7bee2548246ec09
Ciro Duran <ciro.duran@bossalien.com>
2017-02-23 14:11:57 +0000

    Answers for day 3 (part 2 uses ++, though, it can be improved)

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,10 +4,13 @@ module Lib
 
 import Advent1
 import Advent2
+import Advent3
 
 execLib :: String -> String
 execLib x = case x of "advent1_1" -> show advent1_1
                       "advent1_2" -> show advent1_2
                       "advent2_1" -> show advent2_1
                       "advent2_2" -> show advent2_2
+                      "advent3_1" -> show advent3_1
+                      "advent3_2" -> show advent3_2
                       otherwise -> "Failed!"
