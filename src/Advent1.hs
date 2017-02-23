f1f6f8b868852802994c683350bc9a9ee9924108
Ciro Durán <ciro.duran@gmail.com>
2017-02-23 07:45:10 +0000

    advent1_2 gives the correct answer

--- src/Advent1.hs
+++ src/Advent1.hs
@@ -30,7 +30,7 @@ position :: State -> Position
 position (State p r) = p
 
 walkWithSteps :: State -> Int -> [State]
-walkWithSteps state blocks = take blocks $ iterate (step) state
+walkWithSteps state blocks = take (blocks+1) $ iterate (step) state
 
 walk :: State -> Int -> State
 walk state blocks = last $ walkWithSteps state blocks
@@ -68,7 +68,7 @@ toInstruction :: String -> Instruction
 toInstruction (x:xs) = I (read [x]) (read xs)
 
 -- Answers
-
+-- 24 steps -- Blocks 14
 input = "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2"
 start = State (Pos 0 0) North
 
@@ -77,7 +77,7 @@ parsedInstructions = map (toInstruction.trim) $ separate ',' input
 advent1_1 = 
     blocksAwayFromEasterBunnyHQ $ position $ foldl travel start parsedInstructions
 
-advent1_2 = blocksAwayFromEasterBunnyHQ $ position $ firstIntersection $ runWriter $ allTheSteps start parsedInstructions
+advent1_2 = blocksAwayFromEasterBunnyHQ $ firstIntersection $ runWriter $ allTheSteps start parsedInstructions
     where firstIntersection (_,l) = firstIntersection' l []
           firstIntersection' (x:xs) l
               | elem x l = x
